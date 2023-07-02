4e8d0b0173f6b8831ddf7121b5df18a40ed9f836
김민석 <95396532+biber5515@users.noreply.github.com>
2023-07-02 18:58:32 +0900

    멤버VO

--- src/main/java/com/baever/member/vo/member.java
+++ src/main/java/com/baever/member/vo/member.java
@@ -1,4 +1,21 @@
 package com.baever.member.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class member {
+
+    private int idSeq;
+    private String id;
+    private String password;
+    private String name;
+    private int boxSeq;
+    private String boxName;
+    private String email;
+    private String nickName;
+    private String date;
 }
